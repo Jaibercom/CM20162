bb6dc71a42ac86c71aa965ad2288a1a7576cf6cf
jaibercom <jaibercom@gmail.com>
2016-09-14 11:32:53 -0500

    Broadcast Receiver examples

--- BroadcastReceiver/Alarm/src/main/java/co/edu/udea/compumovil/alarm/MainActivity.java
+++ BroadcastReceiver/Alarm/src/main/java/co/edu/udea/compumovil/alarm/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         spinner = (Spinner) findViewById(R.id.spinner);
     }
