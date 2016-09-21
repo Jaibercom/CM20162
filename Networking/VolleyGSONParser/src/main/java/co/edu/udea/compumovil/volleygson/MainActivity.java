429887c5c4789471f6f53141ecc7ebf23613117b
jaibercom <jaibercom@gmail.com>
2016-09-21 12:11:18 -0500

    Networking examples

--- Networking/VolleyGSONParser/src/main/java/co/edu/udea/compumovil/volleygson/MainActivity.java
+++ Networking/VolleyGSONParser/src/main/java/co/edu/udea/compumovil/volleygson/MainActivity.java
@@ -1,9 +1,12 @@
 package co.edu.udea.compumovil.volleygson;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.android.volley.Request.Method;
@@ -25,36 +28,42 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextView;
+    private ProgressDialog Dialog;
+    private ListView listView;
+
     private final String TAG = "VolleyGSON";
 
-    private static final String USER_NAME = "aporter";
-    private static final String PARAMS = "?north=20&south=-20&east=-60&west=-80&username=" + USER_NAME;
-    private static final String REQUEST = "/earthquakesJSON";
-    private static final String BASE_URL = "http://api.geonames.org";
-    private String URL = null;
-
     private RequestQueue requestQueue;
-    private Gson gson;
+
+    private ArrayList<String> result ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTextView = (TextView) findViewById(R.id.textView1);
+        //mTextView = (TextView) findViewById(R.id.textView1);
+        listView = (ListView) findViewById(R.id.listView);
+        result = new ArrayList<String>();
+
     }
 
     public void onClick(View v) {
 
-        URL = BASE_URL+REQUEST+PARAMS;
-        sendRequest();
+        final String USER_NAME = "aporter";
+        final String PARAMS = "?north=20&south=-20&east=-60&west=-80&username=" + USER_NAME;
+        final String REQUEST = "/earthquakesJSON";
+        final String BASE_URL = "http://api.geonames.org";
+        String URL = BASE_URL+REQUEST+PARAMS;
+        sendRequest(URL);
     }
 
-    private void sendRequest() {
+    private void sendRequest(String URL) {
 
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
@@ -72,14 +81,20 @@ public class MainActivity extends AppCompatActivity {
                                 JsonObject jo = (JsonObject) jsonParser.parse(response.toString());
                                 JsonArray jsonArr = jo.getAsJsonArray("earthquakes");
 
+                                Gson gson;
                                 gson = new Gson();
                                 EarthQuake[] earthQuakes = gson.fromJson(jsonArr, EarthQuake[].class);
 
                                 if(earthQuakes != null) {
-                                    //Log.d(TAG, "*earthQuakes: " + earthQuakes.getMagnitude());
                                     for(int i=0;i<earthQuakes.length; i++){
                                         Log.d(TAG, "*earthQuakes: " + earthQuakes[i].getMagnitude());
+                                        result.add("Magnitude: " + earthQuakes[i].getMagnitude()
+                                                + ", Lat :" + earthQuakes[i].getLat()
+                                                + ", Lng :" + earthQuakes[i].getLng());
                                     }
+
+                                    ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(), R.layout.list_item, result);
+                                    listView.setAdapter(adapter);
 
                                 }
                                 else
@@ -90,7 +105,8 @@ public class MainActivity extends AppCompatActivity {
 
                             @Override
                             public void onErrorResponse(VolleyError error) {
-                                mTextView.setText("Error: " + error.toString());
+                                //mTextView.setText("Error: " + error.toString());
+                                Log.d(TAG, "Error: " + error.toString());
 
                             }
                         }
